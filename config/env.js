4f5cdc5fba58ea12a75e071e8e7dd242647be8d0
Sandeep Kumar HR <sandkumar91@gmail.com>
2017-03-21 10:59:26 +0530

    adding db related config change

--- config/env.js
+++ config/env.js
@@ -4,6 +4,7 @@ export const HOST = process.env.HOSTNAME || 'localhost';
 export const PORT = process.env.PORT || '3000';
 export const ENV = process.env.NODE_ENV || 'development';
 
-export const DB_TYPE = process.env.DB_TYPE || DB_TYPES.NONE;
+// Changing DBTypes, cause i am not using any DB also code realted to DB will be removed soon.
+export const DB_TYPE = DB_TYPES.NONE;
 
 export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID || null;
