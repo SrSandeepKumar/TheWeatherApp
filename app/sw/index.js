ad18f8705ad331584d2e3f5fb0928247665da773
Sandeep Kumar HR <sandkumar91@gmail.com>
2017-03-23 21:55:03 +0530

    commiting left out folder - which cause error during deployment

---
+++ app/sw/index.js
@@ -0,0 +1,19 @@
+/* Adding file for service worker */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/serviceWorker.js').then((registration) => {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch((err) => {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+
+  // Install - adding event listener for installing sw
+  self.addEventListener('install', (event) => {
+    // perform actions required on installing
+    console.log(event);
+  });
+
+}
